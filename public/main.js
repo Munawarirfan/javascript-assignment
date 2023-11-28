8709d7aab801485774da756180a1ccb3b1f73991
Munawarirfan <imunawarali0@gmail.com>
2023-11-27 23:42:17 -0800

    completed

---
+++ public/main.js
@@ -0,0 +1,14 @@
+var person1 = parseInt(prompt("Enter Islamiat Marks"));
+var person2 = parseInt(prompt("Enter English Marks"));
+var person3 = parseInt(prompt("Enter Urdu Marks"));
+var person4 = parseInt(prompt("Enter Maths Marks"));
+var person5 = parseInt(prompt("Enter Biology Marks"));
+
+// var person1 = prompt("Enter Islamiat Marks");
+// var person2 = prompt("Enter English Marks");
+// var person3 = prompt("Enter Urdu Marks");
+// var person4 = prompt("Enter Maths Marks");
+// var person5 = prompt("Enter Biology Marks");
+var sum = person1 + person2 + person3 + person4 + person5;
+
+alert(sum);
